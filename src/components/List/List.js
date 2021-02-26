88d57ca2131c6137ffc6ad93c88f688c1f714fa6
Dagmara Gryka <dagagryka@gmail.com>
2021-02-26 17:56:34 +0100

    fix

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,31 +4,34 @@ import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
-
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
-    static propTypes = {
-      title: PropTypes.node.isRequired,
-      description: PropTypes.node,
-      columns: PropTypes.array,
-      image: PropTypes.string.isRequired,
-    }
-    static defaultProps = {
-      description: settings.defaultListDescription,
-    }
-    
+  state = {
+    columns: this.props.columns || [],
+  }
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+    image: PropTypes.string.isRequired,
+  }
+  static defaultProps = {
+    description: settings.defaultListDescription,
+  }
+
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageSource={this.props.image} />
         <div className={styles.description}>
-          {this.props.description}
+          {ReactHtmlParser(this.props.description)}
         </div>
 
         <div className={styles.columns}>
-          <Column titleColumn={'Book'} />
-          <Column titleColumn={'Movie'} />
-          <Column titleColumn={'Photos'} />
+          {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
         
       </section>
