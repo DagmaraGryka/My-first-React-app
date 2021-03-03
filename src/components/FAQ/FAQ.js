cb176c2dce710c94c27211bb2e9b037a4581626f
Dagmara Gryka <dagagryka@gmail.com>
2021-03-03 19:16:48 +0100

    fix styles

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import { faq } from '../../data/dataStore';
+import styles from './FAQ.scss';
 
-const FAQ = () => (
-  <Container>
-    <Hero titleText={faq.title} imageSource={faq.image}/>
-    <h2>{faq.subtitle}</h2>
-    <p>{faq.faqinfo}</p>
+
+const FAQ = () => (    
+ 
+  <Container >
+    <div className={styles.component}>
+      <Hero titleText={faq.title} imageSource={faq.image}/>
+      <h2 className={styles.subtitle}>{faq.subtitle}</h2>
+      <p>{faq.faqinfo}</p>
+    </div>  
   </Container>
 );
 
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import { inform } from '../../data/dataStore';
+import styles from './Info.scss';
 
 const Info = () => (
   <Container>
-    <Hero titleText={inform.title} imageSource={inform.image}/>
-    <h2>{inform.subtitle}</h2>
-    <p>{inform.information}</p>
+    <div className={styles.component}>
+      <Hero titleText={inform.title} imageSource={inform.image}/>
+      <h2 className={styles.subtitle}>{inform.subtitle}</h2>
+      <p>{inform.information}</p>
+    </div>
   </Container>
 );
 
